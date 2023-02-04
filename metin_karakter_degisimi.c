0ce43cf2e073dea4bef681ecdbae7718ea25deac
ensKyhn74 <117343032+ensKyhn74@users.noreply.github.com>
2023-02-04 22:38:35 +0300

    Add files via upload

---
+++ metin_karakter_degisimi.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+// klavyeden metin girilecek,hangi karakterin hangi karakterle değiştirileceği kullanıcıya sorulacak ve yeni metin ekrana yazdırılacak
+// input: bilgisayar bilimi  değişecek karakter: i  değiştirilecek karakter: *
+// output: b*lg*sayar b*l*m* 5 adet i karakteri* ile değiştirildi
+
+int main()
+
+{
+  
+ int KarakterSayisi = 0;  
+   
+ int i = 0;  
+   
+ char input[30];  
+   
+ char DegisecekKarakter;
+ 
+ char DegistirilecekKarakter;
+ 
+ puts("metin girin:");
+ gets(input);
+ 
+ puts("hangi karakteri degistirmek istiyorsunuz?:");
+ scanf("%c",&DegisecekKarakter);
+ 
+ puts("hangi karakter ile degistirmek istiyorsunuz?:");
+ 
+//Buffer her hangi bir hafıza alanında geçici bir kayıt alanıdır(tampon saha),verilerin I/O işlemlerinden sonra belleğe yazılmadan önce uğradıkları bir sahadır.
+ fflush(stdin);  // buffer(arabellek)'i temezledik
+ 
+ scanf(" %c",&DegistirilecekKarakter);
+  
+ while(input[i] != '\0') 
+ {
+    
+   if(input[i] == DegisecekKarakter) 
+   {
+    KarakterSayisi++;
+    
+    input[i] = DegistirilecekKarakter;
+   }
+    
+   i++; 
+ } 
+  
+  puts(input);
+  
+  printf("dizgide %d adet %c karakteri %c ile degistirildi.",KarakterSayisi,DegisecekKarakter,DegistirilecekKarakter);
+ 
+  return 0;
+  
+}
